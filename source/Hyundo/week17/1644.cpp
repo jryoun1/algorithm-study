9c9aaaaa5149b77c115e4c8adc66f29b3b6c63c4
Kimhyundo <kk090303@naver.com>
2020-11-20 15:26:44 +0900

    1644.cpp

--- source/Hyundo/week17/1644.cpp
+++ source/Hyundo/week17/1644.cpp
@@ -1,75 +1,51 @@
 #include<iostream>
 #include<vector>
 
+#define MAX 4000001
+
 using namespace std;
 
-int arr[4000001];
+bool arr[MAX];
 
 vector<int> primeNum;
-vector<int> sumOfPrime;
 
 void aratos() {
 	int i = 2;
-	for (i = 2; i < 4000001; i++) {
-		arr[i] = i;
-	}
-
-	for (i = 2; i < 4000001; i++) {
-		if (arr[i] == 0)
+	arr[1] = 1;
+	for (i = 2; i < MAX; i++) {
+		if (arr[i] == 1)
 			continue;
-		for (int j = i + i; j < 4000001; j += i) {
-			arr[j] = 0;
+		for (int j = i + i; j < MAX; j += i) {
+			arr[j] = 1;
 		}
 	}
-
-	for (int i = 2; i < 4000001; i++)
-	{
-		if (arr[i] != 0)
-			primeNum.push_back(arr[i]);
+	for (int i = 2; i <= MAX; i++) {
+		if (arr[i] == 0) {
+			primeNum.push_back(i);
+		}
 	}
 }
-
-void sumOfPrimeNumber()
-{
-	sumOfPrime.push_back(primeNum[0]);
-	for (int i = 1; i < primeNum.size(); i++)
-	{
-		sumOfPrime.push_back(primeNum[i] + sumOfPrime[i - 1]);
-	}
-}
-
 
 int main()
 {
 	aratos();
-	sumOfPrimeNumber();
-
 	int count=0;
 	int N;
+
 	cin >> N;
-	int tmp=0;
+	int top = 0,bot = 0,sum = 0;
 
-	for (int i = 0; i < primeNum.size(); i++)
-	{
-		if (N < primeNum[i])
-		{
-			tmp = i;
-			break;
+	int maxIdx = primeNum.size();
+	while (bot <= top && top < maxIdx && primeNum[bot] <= N) {
+		if (sum < N) {
+			sum += primeNum[top++];
 		}
+		else if (sum >= N) {
+			sum -= primeNum[bot++];
+		}
+		if (sum == N) count++;
 	}
 
-	for (int i = 0; i<tmp; i++)
-	{
-		for (int j = i+1; j<tmp; j++)
-		{
-			if (sumOfPrime[j] - sumOfPrime[i] == N) {
-				count++;
-			}
-		}
-	}
-
-	if (arr[N] != 0)
-		count++;
 
 	cout << count << endl;
 
