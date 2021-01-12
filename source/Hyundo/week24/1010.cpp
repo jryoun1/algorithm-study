a71f32a1130c6eb7f8265a560bbd2d8cd47fb1a7
Kimhyundo <kk090303@naver.com>
2021-01-13 08:38:41 +0900

    week24

--- source/Hyundo/week24/1010.cpp
+++ source/Hyundo/week24/1010.cpp
@@ -1,33 +1,30 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
 int T, N, M;
+int mem[33][33] = { 0 };
 
-void solve(int nSite, int mSite) {
-
-
-
-
+int dp(int n, int m) {
+	//n이 0일 때, n=m일 때 1 반환, 기저
+	if (n == 0 || n == m)
+		return 1;
+	//이미 계산이 완료된 경우
+	else if (mem[n][m] != 0) {
+		return mem[n][m];
+	}
+	//점화식 dp[n][m] = dp[n-1][m-1] + dp[n][m-1]
+	else {
+		mem[n][m] = dp(n - 1, m - 1) + dp(n, m - 1);
+		return mem[n][m];
+	}
 }
 
+int main() {
 
-int main()
-{
 	cin >> T;
-	for (int i = 0; i < T; i++) {
+	for (int i = 0; i < T; ++i) {
 		cin >> N >> M;
+		cout<<dp(N, M)<<endl;
 	}
-
-	long long sum = 0;
-	for (int k = 17; k > 0; k--) {
-		for (int i = k; i > 0; i--) {
-			for (int j = i; j > 0; j--) {
-				sum += j;
-			}
-		}
-	}
-
-	cout << sum;
 	return 0;
 }
